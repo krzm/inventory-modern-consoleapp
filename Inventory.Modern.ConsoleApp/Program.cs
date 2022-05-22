c3280ec448bfdf0ef75128ec25489291b3fb1751
krzm <kmazanek@gmail.com>
2022-05-22 18:25:28 +0200

    refactor prog class

--- Inventory.Modern.ConsoleApp/Program.cs
+++ Inventory.Modern.ConsoleApp/Program.cs
@@ -1,15 +1,9 @@
 ﻿using DIHelper;
+using Inventory.Modern.ConsoleApp;
 using Unity;
 
-namespace Inventory.Modern.ConsoleApp;
-
-public class Program
-{
-	static void Main(string[] args)
-	{
-		IBootstraper booter = new Bootstraper(
-			new UnityDependencySuite(
-				new UnityContainer().AddExtension(new Diagnostic())));
-		booter.Boot(args);
-	}
-}
+IBootstraper booter = new Bootstraper(
+    new UnityDependencySuite(
+        new UnityContainer()
+            .AddExtension(new Diagnostic())));
+booter.Boot(args);
