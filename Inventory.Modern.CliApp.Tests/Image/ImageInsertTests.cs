18b195548329e2b4a87ad5bdc2f6c980c80a7db0
krzm <kmazanek@gmail.com>
2022-05-28 23:37:05 +0200

    Add image insert test

---
+++ Inventory.Modern.CliApp.Tests/Image/ImageInsertTests.cs
@@ -0,0 +1,42 @@
+using Inventory.Data;
+using Xunit;
+
+namespace Inventory.Modern.CliApp.Tests;
+
+public class ImageInsertTests
+    : ImageTestApi
+{
+    [Fact]
+    public async void Test_Correct_Data_Insert()
+    {
+        var booter = GetBooter();
+        var uow = GetUnitOfWork(booter);
+
+        var transaction = await uow.BeginTransactionAsync();
+        try
+        {
+            AssertImageCount(uow, 0);
+            RunCmd(booter, "category ins test test");
+            RunCmd(booter, "size ins 1 1 1");
+            var category = GetCategory(uow, elementIndex: 0);
+            var size = GetSize(uow, elementIndex: 0);
+            RunCmd(booter, $"item ins test test {category.Id} {size.Id}");
+            var item = GetItem(uow, 0);
+            var path = @"C:\kmazanek@gmail.com\Image\People\IMG_20210211_163337.jpg";
+            RunCmd(booter, $"itemimage ins {item.Id} {path}");
+            AssertImageCount(uow, 1);
+            var data = GetImage(uow, elementIndex: 0);
+            AssertImage(
+                new Image 
+                { 
+                    Path = path
+                    , ItemId = item.Id
+                }
+                , data!);
+        }
+        finally
+        {
+            await transaction.RollbackAsync();
+        }
+    }
+}
---
+++ Inventory.Modern.CliApp.Tests/Image/ImageTestApi.cs
@@ -0,0 +1,39 @@
+using Inventory.Data;
+using Xunit;
+
+namespace Inventory.Modern.CliApp.Tests;
+
+public abstract class ImageTestApi
+    : ItemTestApi
+{
+    protected static IEnumerable<Image>? GetImages(
+        IInventoryUnitOfWork? unitOfWork)
+    {
+        return unitOfWork?.Image?.Get();
+    }
+
+    protected static void AssertImageCount(
+        IInventoryUnitOfWork? repo
+        , int count)
+    {
+        Assert.True(GetCategories(repo)?.Count() == count);
+    }
+
+    protected static Image GetImage(
+        IInventoryUnitOfWork? repo
+        , int elementIndex)
+    {
+        var data = GetImages(repo)?.ElementAt(elementIndex);
+        ArgumentNullException.ThrowIfNull(data);
+        return data;
+    }
+
+    protected static void AssertImage(
+        Image expected
+        , Image acctual)
+    {
+        Assert.True(acctual?.Id > 0);
+        Assert.True(acctual?.Path == expected.Path);
+        Assert.True(acctual?.ItemId == expected.ItemId);
+    }
+}
