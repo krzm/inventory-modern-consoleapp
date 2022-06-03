2f41352daaee5fc2fa6f6a01d7984a6e3be6609e
krzm <kmazanek@gmail.com>
2022-06-04 01:22:17 +0200

    Upd units for size jar use case test

--- Inventory.Modern.CliApp.UseCase.Tests/JarData.cs
+++ Inventory.Modern.CliApp.UseCase.Tests/JarData.cs
@@ -6,7 +6,7 @@ public class JarData
         new List<object[]>
         {
             new object[] { "category", "ins", "Jar", "Cylindrical container made of glass with lid" }
-            , 
-            new object[] { "size", "ins", "-e", "20", "-i", "10", "-v", "0.9", "-s", "my larger jars collection" }
+            , new object[] { "size", "ins", "1", "2", "-e", "20", "-i", "10", "-v", "0.9", "-s", "my larger jars collection" }
+            
         };
 }
